2fcddde410ed3ab2a0059e8174efa513a4f35246
gokay2027 <gokaydinc2000@gmail.com>
2024-01-28 23:27:48 +0300

    Query output model has been created for GetAllProducts

--- ProductManagementAndFinance/Application/Queries/Concrete/ProductQuery.cs
+++ ProductManagementAndFinance/Application/Queries/Concrete/ProductQuery.cs
@@ -1,9 +1,18 @@
 ﻿using ProductManagementAndFinance.Application.Queries.Abstract;
+using ProductManagementAndFinanceData.Repository.EntityRepository.Abstract;
 
 namespace ProductManagementAndFinance.Application.Queries.Concrete
 {
     public class ProductQuery : IProductQuery
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductQuery(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
 
     }
 }
---
+++ ProductManagementAndFinance/Models/Query/GetAllProductsOutputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductManagementAndFinance.Models.Query
+{
+    public class GetAllProductsOutputModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string PriceCurrency { get; set; }
+        public string StorageName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
