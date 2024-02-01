527a2108a2e9423ae16841a5e9b96440108efc28
Gokay DINC <gokaydinc2000@gmail.com>
2024-02-01 16:10:44 +0300

    ..

--- ProductManagementAndFinanceData/ProductManagementAndFinanceContext.cs
+++ ProductManagementAndFinanceData/ProductManagementAndFinanceContext.cs
@@ -6,6 +6,10 @@ namespace ProductManagementAndFinanceData
 {
     public class ProductManagementAndFinanceContext : DbContext
     {
+        public ProductManagementAndFinanceContext()
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
