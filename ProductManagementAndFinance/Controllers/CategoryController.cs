a4b5844673adce8f44205be32ed6460f5404e9f5
gokay2027 <gokaydinc2000@gmail.com>
2024-02-07 18:33:36 +0300

    cateogryController implementation

--- ProductManagementAndFinance/Controllers/CategoryController.cs
+++ ProductManagementAndFinance/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductManagementAndFinance.Application.Queries.Abstract;
+using ProductManagementAndFinanceApi.Application.Commands.Abstract;
+using ProductManagementAndFinanceApi.Models.Command.Category;
 using ProductManagementAndFinanceApi.Models.Query.Category;
 
 namespace ProductManagementAndFinanceApi.Controllers
@@ -9,16 +11,36 @@ namespace ProductManagementAndFinanceApi.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryQuery _categoryQuery;
+        private readonly ICategoryCommandBusiness _categoryCommandBusiness;
 
-        public CategoryController(ICategoryQuery categoryQuery)
+        public CategoryController(ICategoryQuery categoryQuery, ICategoryCommandBusiness categoryCommandBusiness)
         {
             _categoryQuery = categoryQuery;
+            _categoryCommandBusiness = categoryCommandBusiness;
         }
 
         [HttpGet]
         public async Task<CategoryListOutputModel> GetAllCategories()
         {
             return await _categoryQuery.GetAllCategories();
+        }
+
+        [HttpPut]
+        public async Task<UpdateCategoryOutputModel> UpdateCategory([FromQuery] UpdateCategoryModel model)
+        {
+            return await _categoryCommandBusiness.UpdateCategory(model);
+        }
+
+        [HttpPost]
+        public async Task<AddCategoryCommandOutputModel> AddCategory([FromQuery] AddCategoryModel model)
+        {
+            return await _categoryCommandBusiness.AddCategory(model);
+        }
+
+        [HttpDelete]
+        public async Task<DeleteCategoryCommandOutputModel> DeleteCategory([FromQuery] DeleteCategoryModel model)
+        {
+            return await _categoryCommandBusiness.DeleteCategory(model);
         }
     }
 }
