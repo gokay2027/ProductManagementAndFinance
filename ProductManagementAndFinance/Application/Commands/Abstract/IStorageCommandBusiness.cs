503ba7d3290cbfab28bc0fe2cf60ea34650b9992
Gokay DINC <gokaydinc2000@gmail.com>
2024-02-09 12:10:34 +0300

    ..

--- ProductManagementAndFinance/Application/Commands/Abstract/IStorageCommandBusiness.cs
+++ ProductManagementAndFinance/Application/Commands/Abstract/IStorageCommandBusiness.cs
@@ -4,10 +4,25 @@ namespace ProductManagementAndFinanceApi.Application.Commands.Abstract
 {
     public interface IStorageCommandBusiness
     {
+        /// <summary>
+        /// Adding storage
+        /// </summary>
+        /// <param name="inputModel"></param>
+        /// <returns></returns>
         Task<AddStorageOutputModel> AddStorage(AddStorageInputModel inputModel);
 
+        /// <summary>
+        /// Delete storage
+        /// </summary>
+        /// <param name="inputModel"></param>
+        /// <returns></returns>
         Task<DeleteStorageOutputModel> DeleteStorage(DeleteStorageInputModel inputModel);
 
+        /// <summary>
+        /// Update storage
+        /// </summary>
+        /// <param name="inputModel"></param>
+        /// <returns></returns>
         Task<UpdateStorageOutputModel> UpdateStorage(UpdateStorageInputModel inputModel);
     }
 }
